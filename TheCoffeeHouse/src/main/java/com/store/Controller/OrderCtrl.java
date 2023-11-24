36332d75164b2c4ca019ec6e69cc074c96af9d15
PhuocTDP <137909698+PhuocTDP@users.noreply.github.com>
2023-11-24 18:28:25 +0700

    Update OrderCtrl.java Phuoc

--- TheCoffeeHouse/src/main/java/com/store/Controller/OrderCtrl.java
+++ TheCoffeeHouse/src/main/java/com/store/Controller/OrderCtrl.java
@@ -8,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.store.DAO.OrderDAO;
+import com.store.entity.Comment;
+import com.store.entity.Order;
 import com.store.service.OrderService;
 
 
@@ -15,6 +18,10 @@ import com.store.service.OrderService;
 public class OrderCtrl {
 	@Autowired
 	OrderService orderService;
+	
+	@Autowired
+	OrderDAO dao;
+	
 	
 	@RequestMapping("/order/checkout")
 	public String checkout() {
@@ -33,4 +40,27 @@ public class OrderCtrl {
 		model.addAttribute("order", orderService.findById(id));
 		return "order/detail";
 	}
+	
+	@RequestMapping("/order/cancel/{id}") // Huỷ đơn hàng
+    public String cancel(@PathVariable("id") Long id) {
+        Order order = orderService.findById(id);
+        order.setAction("Đã huỷ xác nhận");
+        dao.save(order);
+        return "redirect:/order/list";
+    }
+
+    @RequestMapping("/order/receive/{id}") // Đã nhận được hàng
+    public String receive(@PathVariable("id") Long id, Model model, Order order) {
+        order = orderService.findById(id);
+        dao.save(order);
+
+        // Thêm thông tin cần thiết vào model, ví dụ: orderID
+        model.addAttribute("orderId", id);
+
+        // Chuyển hướng đến trang đánh giá sản phẩm
+        return "product/review";
+    }
+    
+
+
 }
